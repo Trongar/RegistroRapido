18a79a24d158fe75cbbc2c84e7d8f0e092f428dd
Trongar <sanchesedgar97@gmail.com>
2023-12-23 16:04:12 -0600

    gasales

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Appwrite + Astro = ❤️";
+export const SITE_DESCRIPTION =
+  "An example blog demonstrating how to use Astro with Appwrite.";
