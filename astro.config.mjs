3e26d9f913b82ceb63e3e7d1dfb0059e61b101f8
Trongar <sancheseddgar97@gmail.com>
2024-01-11 01:31:46 +0000

    initialize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,4 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
-
-import svelte from "@astrojs/svelte";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  site: "https://appwrite-demos-for-astro-blog.vercel.app",
-  integrations: [mdx(), sitemap(), svelte()],
-  adapter: vercel(),
-});
+export default defineConfig({});
--- src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Gasales";
-export const SITE_DESCRIPTION =
-  "An example blog demonstrating how to use Astro with Appwrite.";
