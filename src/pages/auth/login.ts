c8284f311877300f45a0902f26186049c2ab8821
Trongar <sanchesedgar97@gmail.com>
2024-01-14 12:12:54 -0600

    Add login functionality to APIRoute

---
+++ src/pages/auth/login.ts
@@ -0,0 +1,61 @@
+import * as setCookie from "set-cookie-parser";
+import {
+  AppwriteEndpoint,
+  SsrHostname,
+  AppwriteHostname,
+  AppwriteProject,
+} from "@lib/appwrite";
+
+import type { APIRoute } from "astro"
+
+
+export const POST: APIRoute = async ({ cookies }) => {
+  const response = await fetch(
+    `${AppwriteEndpoint}/account/sessions/anonymous`,
+    {
+      method: "POST",
+      headers: {
+        "x-appwrite-project": AppwriteProject,
+      },
+    }
+  );
+
+
+  const json = await response.json();
+
+  if (json.code >= 400) {
+    return new Response(JSON.stringify({ message: json.message }))
+  }
+  const ssrHostname =
+    SsrHostname === "localhost" ? SsrHostname : "." + SsrHostname;
+  const appwriteHostname =
+    AppwriteHostname === "localhost"
+      ? AppwriteHostname
+      : "." + AppwriteHostname;
+
+  const cookiesStr = (response.headers.get("set-cookie") ?? "")
+    .split(appwriteHostname)
+    .join(ssrHostname);
+
+  const cookiesArray = setCookie.splitCookiesString(cookiesStr);
+
+  const cookiesParsed = cookiesArray.map((cookie) =>
+    setCookie.parseString(cookie)
+  );
+
+  for (const cookie of cookiesParsed) {
+    console.log(cookie.name);
+
+    cookies.set(cookie.name, cookie.value, {
+      domain: cookie.domain,
+      secure: cookie.secure,
+      sameSite: cookie.sameSite,
+      path: cookie.path,
+      maxAge: cookie.maxAge,
+      httpOnly: cookie.httpOnly,
+      expires: cookie.expires,
+    });
+  }
+
+  return new Response(JSON.stringify({json}))
+}
